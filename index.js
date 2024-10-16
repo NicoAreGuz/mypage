44f0fe7431e3cc1e52a89602f3821485d359c02c
Nicolas Arellano Guzman <narel001@campus.goldmsmiths.ac.uk>
2024-10-16 12:00:56 +0100

    ver0.4

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //config settings
 
 var http = require("http");
-const port = 5500;
-const path = require ("path");
+const port = 5501;
 
 http.createServer(function(req, res) {
-    res.sendFile(path.join(__mypage,'index.html'));
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`);
 }); 
